7b2c2a511330b6b17d57f9cb60ea65af93c085a6
shubhrajyoti <shubhrajyoti.php@gmail.com>
2021-01-03 20:42:36 +0530

    child routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,42 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent} from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BugsComponent } from './bugs/bugs.component';
+import { DetailsComponent } from './bugs/details/details.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { AddNewComponent } from './bugs/add-new/add-new.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'bugs',
+    children: [
+      {
+        path: '',
+        component: BugsComponent
+      },
+      {
+        path: 'details/:id',
+        component: DetailsComponent
+      },
+      {
+        path: 'add-new',
+        component: AddNewComponent
+      }
+    ]
+
+  },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NoPageFoundComponent }
+];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes)],
-  imports: [
-    RouterModule.forRoot([
-      {path: 'login', component: LoginComponent}
-    ]),
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,25 @@ import { HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BugsComponent } from './bugs/bugs.component';
+// import { BugDetailsComponent } from './bug-details/bug-details.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { AddNewComponent } from './bugs/add-new/add-new.component';
+import { DetailsComponent } from './bugs/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AboutComponent,
+    ContactComponent,
+    BugsComponent,
+    // BugDetailsComponent,
+    NoPageFoundComponent,
+    AddNewComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
